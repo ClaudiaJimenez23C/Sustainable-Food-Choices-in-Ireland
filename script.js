3cacec5c28cc84ba21d0c100d35a8cd8acf8b24e
ClaudiaJimenez23C <x24318612@student.ncirl.ie>
2026-08-04 22:44:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,63 @@
+const button = document.getElementById("calculateBtn");
+
+const bagsInput = document.getElementById("bags");
+
+const result = document.getElementById("result1");
+
+const error = document.getElementById("errorMessage");
+
+
+button.addEventListener("click", function(){
+
+
+    // Get the number entered by the user
+
+    let bags = Number(bagsInput.value);
+
+
+
+    // Validation
+
+    if (bagsInput.value === "") {
+
+        error.innerHTML = 
+        "Please enter the number of bags.";
+
+        result.innerHTML = "";
+
+        return;
+    }
+
+
+
+    if (bags < 0) {
+
+        error.innerHTML =
+        "Number of bags cannot be negative.";
+
+        result.innerHTML = "";
+
+        return;
+    }
+
+
+
+    // Calculation
+
+    let co2 = bags * 20;
+
+
+
+    // Display result
+
+    result.innerHTML =
+    "Your plastic bags produce approximately "
+    + co2
+    + " grams of CO₂.";
+
+
+
+    error.innerHTML = "";
+
+
+});
